722cf2f74026d737bccea5b068af7b1dfe6d46cd
pedrohtserra <pepeserra2023@gmail.com>
2025-03-13 14:29:31 -0300

    Adicionado Juros Compostos Diario

---
+++ Exercicio 3 - Juros Compostos/juroscompostodiario.c
@@ -0,0 +1,36 @@
+// O juros é apenas aplicado nos dias úteis, por isso dias = 22
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    double valormensal = 0;
+    double taxadejuros = 0;
+    double valortotal = 0;
+    int anos = 0;
+    int dias = 22;
+    int meses = 1;
+    
+    printf("Investir por quantos anos? ");
+    scanf("%d", &anos);
+    printf("Investir quanto mensalmente? ");
+    scanf("%lf", &valormensal);
+    printf("Qual a taxa de juros? ");
+    scanf("%lf", &taxadejuros);
+
+    for (int k = 0; k < anos; k++)
+    {
+        for (int j = 0; j < meses; j++)
+        {   
+            valortotal += valormensal;
+            for (int i = 0; i < dias; i++)
+            {
+                valortotal = valortotal * ((double) 1 + taxadejuros / (double) 100);
+            }
+        }
+    }
+
+    printf("\nValor total: %.2lf\n", valortotal);
+    return 0;
+}
