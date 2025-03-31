e8944fa014e978e3a9df6e8247cbdc6ee642cbe9
pedrohtserra <pepeserra2023@gmail.com>
2025-03-31 19:10:29 -0300

    Atualizado Batalha Naval (incompleto)

--- Primeiro Periodo/Exercicio 9 - Batalha Naval/batalhanaval.c
+++ Primeiro Periodo/Exercicio 9 - Batalha Naval/batalhanaval.c
@@ -4,9 +4,9 @@
 
 #define tamanho 15
 
-void iniciarmapa(int mapa1[tamanho][tamanho], int mapa2[tamanho][tamanho]);
-void definirmapa(int mapa1[tamanho][tamanho]);
-void imprimirmapa(int mapa[tamanho][tamanho], int jogador);
+void iniciar_mapa(int mapa1[tamanho][tamanho], int mapa2[tamanho][tamanho]);
+void definir_mapa(int mapa1[tamanho][tamanho], int jogador);
+void imprimir_mapa(int mapa[tamanho][tamanho], int jogador);
 
 int main(void)
 {
@@ -15,18 +15,18 @@ int main(void)
     int jogador1 = 1;
     int jogador2 = 2;
 
-    iniciarmapa(mapa1, mapa2); // criando um mapa para cada jogador
+    iniciar_mapa(mapa1, mapa2); // criando um mapa para cada jogador
 
-    definirmapa(mapa1); // definir mapa do jogador 1
-    imprimirmapa(mapa1, jogador1); // teste para ver se os barcos estão nos seus devidos lugares
+    definir_mapa(mapa1, jogador1); // definir mapa do jogador 1
+    imprimir_mapa(mapa1, jogador1); // teste para ver se os barcos estão nos seus devidos lugares
     
-    definirmapa(mapa2); // definir mapa do jogador 2
-    imprimirmapa(mapa2, jogador2); // teste para ver se os barcos estão nos seus devidos lugares novamente
+    definir_mapa(mapa2, jogador2); // definir mapa do jogador 2
+    imprimir_mapa(mapa2, jogador2); // teste para ver se os barcos estão nos seus devidos lugares novamente
 
     return 0;
 }
 
-void iniciarmapa(int mapa1[tamanho][tamanho], int mapa2[tamanho][tamanho])
+void iniciar_mapa(int mapa1[tamanho][tamanho], int mapa2[tamanho][tamanho])
 {
     for (int i = 0; i < tamanho; i++)
     {
@@ -38,35 +38,40 @@ void iniciarmapa(int mapa1[tamanho][tamanho], int mapa2[tamanho][tamanho])
     }
 }
 
-void definirmapa(int mapa1[tamanho][tamanho])
+void definir_mapa(int mapa[tamanho][tamanho], int jogador)
 {
-    int barcos[4] = {5, 4, 3, 2}; // criando 4 tipos de barco, com seus respectivos tamanhos
+    int navios[4] = {5, 4, 3, 2}; // criando 4 tipos de navio, com seus respectivos tamanhos
     int linha = 0;
     int coluna = 0; 
     int verhor = 0; // resposta se é vertical ou horizontal (0) se for horizontal e (1) se for vertical
 
+    printf("\nVez do jogador %d\n", jogador);
+
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < i + 1; j++) {
-            int tamanhoBarco = barcos[i];
+            int tamanhoNavio = navios[i];
 
-            printf("\nEscolha uma posição para o barco %d de tamanho %d:\n", j + 1, tamanhoBarco);
+            printf("\nEscolha uma posição para o navio %d de tamanho %d:\n", j + 1, tamanhoNavio);
             
             // escolher linha
             do {
                 printf("Linha (1 a 15): ");
                 scanf("%d", &linha);
+                printf("\n");
             } while (linha < 1 || linha > tamanho);
             
             // escolher coluna
             do {
                 printf("Coluna (1 a 15): ");
                 scanf("%d", &coluna);
+                printf("\n");
             } while (coluna < 1 || coluna > tamanho);
 
             // escolher entre vertical e horizontal
             do {
                 printf("Se for horizontal digite (0), se for vertical digite (1): ");
                 scanf("%d", &verhor);
+                printf("\n");
             } while (verhor != 0 && verhor != 1);
 
             // ajustando a linha e a coluna para indicar que começa na posição 0
@@ -77,44 +82,44 @@ void definirmapa(int mapa1[tamanho][tamanho])
             bool podeColocar = true;
 
             if (verhor == 0) { // horizontal
-                if (coluna + tamanhoBarco > tamanho) {
-                    printf("O barco ultrapassa os limites do mapa! Escolha outra posição.\n\n");
+                if (coluna + tamanhoNavio > tamanho) {
+                    printf("O navio ultrapassa os limites do mapa! Escolha outra posição.\n\n");
                     j--; // repete a pergunta de linha, coluna, vertical ou horizontal
                     continue;
                 }
 
-                for (int k = 0; k < tamanhoBarco; k++) {
-                    if (mapa1[linha][coluna + k] != 0) {
-                        podeColocar = 0; // se existe um barco nessa posição, podeColocar = 0, indicando que não pode colocar o novo barco nessa posição
+                for (int k = 0; k < tamanhoNavio; k++) {
+                    if (mapa[linha][coluna + k] != 0) {
+                        podeColocar = false; // se existe um barco nessa posição, podeColocar = 0, indicando que não pode colocar o novo barco nessa posição
                         break;
                     }
                 }
 
-                if (podeColocar) {
-                    for (int k = 0; k < tamanhoBarco; k++) {
-                        mapa1[linha][coluna + k] = tamanhoBarco;
+                if (podeColocar) { // se valor de podeColocar for true, o programa irá colocar o navio
+                    for (int k = 0; k < tamanhoNavio; k++) {
+                        mapa[linha][coluna + k] = tamanhoNavio;
                     }
-                } else {
+                } else { // se o valor for false, o programa não ira colocar o navio e irá repetir a pergunta
                     printf("Posição ocupada, escolha outra posição.\n\n");
                     j--; 
                 }
             } else { // vertical
-                if (linha + tamanhoBarco > tamanho) {
-                    printf("O barco ultrapassa os limites do mapa! Escolha outra posição.\n");
+                if (linha + tamanhoNavio > tamanho) {
+                    printf("O navio ultrapassa os limites do mapa! Escolha outra posição.\n");
                     j--;
                     continue;
                 }
 
-                for (int k = 0; k < tamanhoBarco; k++) {
-                    if (mapa1[linha + k][coluna] != 0) {
-                        podeColocar = 0; 
+                for (int k = 0; k < tamanhoNavio; k++) {
+                    if (mapa[linha + k][coluna] != 0) {
+                        podeColocar = false; 
                         break;
                     }
                 }
 
                 if (podeColocar) {
-                    for (int k = 0; k < tamanhoBarco; k++) {
-                        mapa1[linha + k][coluna] = tamanhoBarco;
+                    for (int k = 0; k < tamanhoNavio; k++) {
+                        mapa[linha + k][coluna] = tamanhoNavio;
                     }
                 } else {
                     printf("Posição ocupada, escolha outra posição.\n\n");
@@ -125,12 +130,12 @@ void definirmapa(int mapa1[tamanho][tamanho])
     }
 }
 
-void imprimirmapa(int mapa[tamanho][tamanho], int jogador) 
+void imprimir_mapa(int mapa[tamanho][tamanho], int jogador) 
 {
     printf("\nMapa Atual do Jogador %d:\n", jogador);
     for (int i = 0; i < tamanho; i++) {
         for (int j = 0; j < tamanho; j++) {
-            printf("%d ", mapa[i][j]);
+            printf("%d  ", mapa[i][j]);
         }
         printf("\n");
     }
